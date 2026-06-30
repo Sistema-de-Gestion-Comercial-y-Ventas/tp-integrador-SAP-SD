95b55a99b8f6c9c08a503b7e379357a3ab90ed72
Facundo Sosa <facundososa284@gmail.com>
2026-06-30 00:04:26 -0300

    Agregar modulo inicial de ventas

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,13 @@ from django.urls import path
 
 from products.controllers.product_controller import ProductController
 from products.controllers.client_controller import ClientController
+from products.controllers.sale_controller import SaleController
 
 
 # Se crea UNA SOLA instancia del controller a nivel de módulo. Esto evita crear una nueva instancia en cada petición HTTP.
 controller = ProductController()
 client_controller = ClientController()
+sale_controller = SaleController()
 
 urlpatterns = [
     # Panel de administración de Django (viene por defecto)
@@ -19,4 +21,7 @@ urlpatterns = [
     path('products/<int:product_id>/', controller.get_product_by_id),
 
     path('clients/', client_controller.get_clients),
+
+    path('sales/', sale_controller.get_sales),
+    path('sales/<int:sale_id>/', sale_controller.get_sale_by_id),
 ]
